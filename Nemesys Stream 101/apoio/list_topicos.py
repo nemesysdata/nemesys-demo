70ee55c172615ef58418f57f0bf926576faf6319
Fernando Adriano Machado <adriano@nemesysdata.com>
2024-02-13 13:41:59 +0000

    feat: added a kafka test app in python

--- Nemesys Stream 101/apoio/list_topicos.py
+++ Nemesys Stream 101/apoio/list_topicos.py
@@ -1,12 +1,23 @@
 import os
+import socket
 from kafka import KafkaConsumer, TopicPartition
 
 KAFKA_BOOTSTRAP = [
-    "127.0.0.1:9094"
+    # "127.0.0.1:9094"
+    "nemesys-stream-101-kafka-bootstrap.nemesys-stream-101.svc:9093"
 ]
 
+def checkKafka(host, port):
+    print("Connecting to host")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((host, port))
+            print("Connected")
+        except Exception as e:
+            print(e)
+        
 def topics():
-    cluster = KafkaConsumer(bootstrap_servers=KAFKA_BOOTSTRAP)
+    cluster = KafkaConsumer(security_protocol="SSL", ssl_check_hostname=False, bootstrap_servers=KAFKA_BOOTSTRAP)
 
     # brokers = cluster.brokers()
     topicos = cluster.topics()
@@ -19,13 +30,13 @@ def topics():
     return lista_topicos
 
 def main():
+    checkKafka("nemesys-stream-101-kafka-bootstrap.nemesys-stream-101.svc", 9093)
     print("list_topics")
     topicos = topics()
     print(f"Encontrado {len(topicos)} topicos")
 
     for topico in topicos:
         print(topico)
-
 
 if __name__ == "__main__":
     main()

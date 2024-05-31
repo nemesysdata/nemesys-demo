18bb66af686277e098c4cc52e5a12122a3c8a9a6
Fernando Adriano Machado <adriano@nemesysdata.com>
2024-05-31 10:19:51 -0300

    feat: add kafka avro suport to sent stocks data

--- Nemesys_Stream_101/DownloadStocks/go.mod
+++ Nemesys_Stream_101/DownloadStocks/go.mod
@@ -4,11 +4,12 @@ go 1.21.6
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/linkedin/goavro/v2 v2.13.0
 	github.com/segmentio/kafka-go v0.4.47
-	go.mongodb.org/mongo-driver v1.13.1
 )
 
 require (
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 )
--- Nemesys_Stream_101/DownloadStocks/main.go
+++ Nemesys_Stream_101/DownloadStocks/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"baixar_stock/finazon"
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/linkedin/goavro/v2"
 	"github.com/segmentio/kafka-go"
 )
 
 var fin *finazon.Finazon
 
+const WORKERS = 100
+
 func worker(jobs <-chan finazon.Stock, results chan<- bool) {
+	// Kafka
 	host := os.Getenv("KAFKA_BOOTSTRAP") //"127.0.0.1:9092"
 	topic := os.Getenv("KAFKA_TOPIC")    //"stocks_intraday"
 
@@ -25,10 +28,52 @@ func worker(jobs <-chan finazon.Stock, results chan<- bool) {
 		return
 	}
 
+	defer conn.Close()
+
 	fmt.Println("Connected to Kafka Server")
+	//
+	// Avro Schema & Codec Definition
+	//
+	codec, err := goavro.NewCodec(`
+		{
+			"type": "record",
+			"name": "Stock",
+			"fields": [
+				{"name": "ticker", "type": "string"},
+				{"name": "timestamp", "type": "string"},
+				{"name": "open", "type": "double"},
+				{"name": "high", "type": "double"},
+				{"name": "low", "type": "double"},
+				{"name": "close", "type": "double"},
+				{"name": "volume", "type": "long"}
+			]
+		}
+	`)
+	if err != nil {
+		fmt.Println("Failed to create Avro codec: ", err)
+		return
+	}
+
 	for stock := range jobs {
-		msg, err := json.Marshal(stock)
+		// json_msg, err := json.Marshal(stock)
+		// if err != nil {
+		// 	fmt.Printf("Failed to encode stock into json: %s\n", err)
+		// 	results <- false
+		// 	continue
+		// }
+
+		// fmt.Println(stock.Timestamp.Format(time.RFC3339))
+		msg, err := codec.BinaryFromNative(nil, map[string]interface{}{
+			"ticker":    stock.Ticker,
+			"timestamp": stock.Timestamp.Format(time.RFC3339),
+			"open":      stock.Open,
+			"high":      stock.High,
+			"low":       stock.Low,
+			"close":     stock.Close,
+			"volume":    stock.Volume,
+		})
 		if err != nil {
+			fmt.Printf("Failed to encode stock into avro: %s\n", err)
 			results <- false
 			continue
 		}
@@ -40,15 +85,13 @@ func worker(jobs <-chan finazon.Stock, results chan<- bool) {
 			results <- true
 		}
 	}
-
-	conn.Close()
 }
 
 func PersistStocks(stocks []finazon.Stock) (int, error) {
 	jobs := make(chan finazon.Stock, len(stocks))
 	results := make(chan bool, len(stocks))
 
-	for w := 1; w <= 100; w++ {
+	for w := 1; w <= WORKERS; w++ {
 		go worker(jobs, results)
 	}
 
@@ -68,6 +111,7 @@ func PersistStocks(stocks []finazon.Stock) (int, error) {
 		}
 	}
 
+	close(results)
 	return persisted_no, nil
 }
 func main() {
@@ -83,8 +127,9 @@ func main() {
 
 	fin = finazon.NewFinazon(finazonAPIKey)
 
+	// for dia := 1; dia <= 29; dia++ {
 	today := time.Now()
-	// today := time.Date(2024, 5, 24, 0, 0, 0, 0, time.FixedZone("UTC-3", -3*60*60))
+	// today := time.Date(2024, 5, dia, 0, 0, 0, 0, time.FixedZone("UTC-3", -3*60*60))
 	fmt.Println("Today is", today.Format("2006-01-02"))
 
 	for dia := today.Day(); dia <= today.Day(); dia++ {
@@ -112,4 +157,5 @@ func main() {
 		fmt.Println(qtd, "/", len(stocks), "persisted entries.")
 		fmt.Println()
 	}
+	// }
 }
